e626a6bfadf80492b45cb2f55effebd8379e03b5
xiaoTao <penghaitao918@163.com>
2016-02-19 12:06:49 +0800

    # 网络架构优化

--- app/src/main/java/com/xiaotao/acmfamily/service/ClientService.java
+++ app/src/main/java/com/xiaotao/acmfamily/service/ClientService.java
@@ -42,7 +42,6 @@ import java.net.SocketTimeoutException;
  */
 public class ClientService extends Service
 {
-
     //  Socket 连接
     private static Socket socket = null;
     // 定义与服务器通信的子线程
@@ -51,11 +50,6 @@ public class ClientService extends Service
     private ServiceReceiver serviceReceiver = null;
     //  将客户端数据发送到服务器
     private ClientSend send = null;
-
-
-    // 该线程所处理的Socket所对应的输入流
-    private BufferedReader br = null;
-    private ClientReceive receive = null;
 
     @Override
     public IBinder onBind(Intent intent)
@@ -76,111 +70,105 @@ public class ClientService extends Service
         new Thread(clientThread).start();
     }
 
-    private void setSocket() {
-        try{
-            socket = new Socket(AppUtil.net.IP, AppUtil.net.port);
-            receiveThread();
-        }
-        catch (SocketTimeoutException timeException){
-            Log.i(AppUtil.tag.network, "ClientService is Error -----> 服务器连接超时");
-            timeException.printStackTrace();
-        }
-        catch (Exception e){
-            Log.i(AppUtil.tag.network, "ClientService is Error -----> 服务器连接失败");
-            e.printStackTrace();
-        }
-    }
-
-
-    public class ClientThread implements Runnable {
-        public ClientThread() {}
-        public void run()
-        {
-            if (socket == null){
-                setSocket();
-                socketCheck().start();
-            }
-        }
-    }
-
-    //  创建一个子线程来读取服务器的相应
-    private void receiveThread(){
-        new Thread() {
-            @Override
-            public void run() {
-                try {
-                    Log.i(AppUtil.tag.error, "ClientService.receiveThread() is Connect");
-                    br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-                    // 启动一条子线程来读取服务器响应的数据
-                    receive = new ClientReceive(ClientService.this, br);
-                    new Thread(receive).start();
-                    // 为当前线程初始化Looper
-                    Looper.prepare();
-                    // 启动Looper
-                    Looper.loop();
-                } catch (Exception e) {
-                    Log.i(AppUtil.tag.error, "ClientService.receiveThread() is Error ----->  Exception ");
-                    e.printStackTrace();
-                }
-            }
-        }.start();
-    }
-
-
-
-
-
-
-    //	将用户的请求提交到网络
-    private void getSend(JSONObject jsonObject) {
-        send = new ClientSend(jsonObject);
-        new Thread(send).start();
-    }
-
-
     public static Socket getSocket() {
         return socket;
     }
 
-    //  心跳检测
-    private Thread socketCheck() {
-        Thread thread = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                while (true) {
-                    boolean flag = true;
-                    while (flag) {
-                        try {
-                            // 发送心跳包
-                            socket.sendUrgentData(0xFF);
-                            System.out.println("目前是处于链接状态！");
-                            Thread.sleep(30 * 1000);
-                        } catch (Exception e) {
-                            System.out.println("目前是处于断开状态！");
-                            socket = null;
-                            flag = false;
-                        }
+    public class ClientThread implements Runnable {
+        // 该线程所处理的Socket所对应的输入流
+        private BufferedReader br = null;
+        private ClientReceive receive = null;
+        public ClientThread() {}
+        @Override
+        public void run()
+        {
+            if (socket == null){
+                setSocket();
+                socketCheck();
+            }
+        }
+        //  创建Socket连接
+        private void setSocket() {
+            try{
+                socket = new Socket(AppUtil.net.IP, AppUtil.net.port);
+                receiveThread();
+            }
+            catch (SocketTimeoutException timeException){
+                Log.i(AppUtil.tag.network, "ClientService is Error -----> 服务器连接超时");
+                timeException.printStackTrace();
+            }
+            catch (Exception e){
+                Log.i(AppUtil.tag.network, "ClientService is Error -----> 服务器连接失败");
+                e.printStackTrace();
+            }
+        }
+        //  创建一个子线程来读取服务器的相应
+        private void receiveThread(){
+            new Thread() {
+                @Override
+                public void run() {
+                    try {
+                        Log.i(AppUtil.tag.error, "ClientService.receiveThread() is Connect");
+                        br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                        // 启动一条子线程来读取服务器响应的数据
+                        receive = new ClientReceive(ClientService.this, br);
+                        new Thread(receive).start();
+                        // 为当前线程初始化Looper
+                        Looper.prepare();
+                        // 启动Looper
+                        Looper.loop();
+                    } catch (Exception e) {
+                        Log.i(AppUtil.tag.error, "ClientService.receiveThread() is Error ----->  Exception ");
+                        e.printStackTrace();
                     }
-                    //    Thread.sleep(3000);
-                    setSocket();
                 }
-            };
-        });
-        return thread;
+            }.start();
+        }
+        //  心跳检测
+        private void socketCheck() {
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    while (true) {
+                        boolean flag = true;
+                        while (flag) {
+                            try {
+                                // 发送心跳包
+                                socket.sendUrgentData(0xFF);
+                                System.out.println("目前是处于链接状态！");
+                                Thread.sleep(30 * 1000);
+                            } catch (Exception e) {
+                                System.out.println("目前是处于断开状态！");
+                                socket = null;
+                                flag = false;
+                            }
+                        }
+                        try {
+                            Thread.sleep(3000);
+                        }catch (Exception e){
+                            e.printStackTrace();
+                        }
+                        setSocket();
+                    }
+                };
+            }).start();
+        }
     }
 
-    //  服务器广播
+    //  服务器广播,将接受到的客户端请求发送到服务器
     public class ServiceReceiver extends BroadcastReceiver {
         @Override
         public void onReceive(final Context context, Intent intent) {
             String msg = intent.getStringExtra(AppUtil.message.service);
             try {
                 JSONObject jsonObject = new JSONObject(msg);
-                getSend(jsonObject);
+                send = new ClientSend(jsonObject);
+                new Thread(send).start();
             }catch (JSONException e){
                 e.printStackTrace();
             }
         }
     }
+
 }
 
